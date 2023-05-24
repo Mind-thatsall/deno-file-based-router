b84f7df322de4b8497d4ee69fed0f54e63ffe31a
Mathieu Rossi <mathieurossi77@gmail.com>
2023-05-24 16:54:19 +0200

    able to remove a route now and nested routes working

--- main.ts
+++ main.ts
@@ -1,26 +1,24 @@
 import * as http from "https://deno.land/std@0.188.0/http/mod.ts";
 import { debounce } from "https://deno.land/std@0.188.0/async/debounce.ts";
 
-const routes: URLPattern[] = [];
+const routes: Array<URLPattern> = [];
 routes.push(new URLPattern({ pathname: "/" }));
 
 async function handler(req: Request) {
   for (const route in routes) {
-    console.log(routes[route]);
-    if (routes[route].exec(req.url)) {
-      if (routes[route].pathname === "/") {
-        return new Response(
-          await Deno.readFile(`./routes${routes[route].pathname}index.html`),
-          { status: 200 },
-        );
-      } else {
-        return new Response(
-          await Deno.readFile(`./routes${routes[route].pathname}/index.html`),
-          { status: 200 },
-        );
-      }
+    if (routes[route].exec(req.url) && routes[route].pathname === "/") {
+      return new Response(
+        await Deno.readFile(`./routes${routes[route].pathname}index.html`),
+        { status: 200 },
+      );
+    } else if (routes[route].exec(req.url)) {
+      return new Response(
+        await Deno.readFile(`./routes${routes[route].pathname}/index.html`),
+        { status: 200 },
+      );
     }
   }
+
   return new Response("404", { status: 404 });
 }
 
@@ -29,16 +27,23 @@ http.serve(
   { port: 3000 },
 );
 
-function createRoutes(eventName: string, pathname: string[]) {
+function createOrDeleteRoutes(eventName: string, pathname: string[]) {
   const fullPathName = pathname.slice(pathname.indexOf("routes") + 1).join(
     "/",
   );
-  if (
-    eventName === "create" && !pathname[pathname.length - 1].includes(".html")
-  ) {
-    routes.push(
-      new URLPattern({ pathname: `/${fullPathName}` }),
-    );
+
+  const indexOfPathName = routes.findIndex((route) =>
+    route.pathname === `/${fullPathName}`
+  );
+
+  // check the extension of file to see if it's a directory or a file
+  const newDirectory = !pathname[pathname.length - 1].includes(".html");
+
+  if (eventName === "create" && newDirectory) {
+    routes.push(new URLPattern({ pathname: `/${fullPathName}` }));
+    console.log(routes);
+  } else if (eventName === "remove" && indexOfPathName !== -1) {
+    routes.splice(indexOfPathName, 1);
     console.log(routes);
   }
 }
@@ -52,6 +57,6 @@ const watcher = Deno.watchFs("./");
 for await (const ev of watcher) {
   log(ev);
   if (ev.paths[0].includes("routes")) {
-    createRoutes(ev.kind, ev.paths[0].split("/"));
+    createOrDeleteRoutes(ev.kind, ev.paths[0].split("/"));
   }
 }
